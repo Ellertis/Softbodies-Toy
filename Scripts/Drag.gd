c10377b3a4278404b60fde9c8bb12b2a9a6933c4
ellertis <emilantonov211@gmail.com>
2026-06-20 12:23:18 +0200

    Test shake directional acceleration

--- Scripts/Drag.gd
+++ Scripts/Drag.gd
@@ -8,47 +8,45 @@ var last_mouse_pos:Vector2 = Vector2.ZERO
 var mouse_velocity:Vector2 = Vector2.ZERO
 
 var bones:Array[RigidBody2D]
-var bdragging:bool = false 
 var touchpos:Vector2 = Vector2.ZERO
 var displacements:PackedVector2Array
 
 func _physics_process(delta):
 	mouse_velocity = (touchpos - last_mouse_pos) / delta
 	last_mouse_pos = touchpos
-	if bdragging:
-		for i in bones.size():
-			var bone = bones[i]
-			var target = touchpos + displacements[i]
-			var offset = target - bone.global_position
-			var spring_force = offset * drag_strength
-			var damping_force = -bone.linear_velocity * damping
-			bone.apply_force(spring_force + damping_force, -mouse_velocity * rotation_damping_factor)
+	for i in bones.size():
+		var bone:RigidBody2D = bones[i]
+		var target:Vector2 = touchpos + displacements[i]
+		var offset:Vector2 = target - bone.global_position
+		var spring_force:Vector2 = offset * drag_strength
+		var damping_force:Vector2 = -bone.linear_velocity * damping
+		bone.apply_force(spring_force + damping_force, -mouse_velocity * rotation_damping_factor)
 
 func _unhandled_input(event):
 	#InputEventMouseButton
 	#InputEventScreenTouch
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			bones.clear()
-			displacements.clear()
 			var body = get_body_under_mouse()
 			if body is RigidBody2D && body.get_parent() is SoftBody2D:
 				print("Clicked:", body.name)
 				_get_displacements(body.get_parent(),body)
-			bdragging = true
-			touchpos = event.position
+				touchpos = event.position
+				set_physics_process(true)
 		if not event.is_pressed():
-			bdragging = false
+			set_physics_process(false)
 			for bone in bones:
 				bone.apply_central_force(mouse_velocity)
+			bones.clear()
+			displacements.clear()
 			print("Released")
 	
-	if bdragging && event is InputEventMouse:
+	if event is InputEventMouse:
 		touchpos = event.position
 
 func get_body_under_mouse():
-	var mouse_pos = get_global_mouse_position()
-	var query = PhysicsPointQueryParameters2D.new()
+	var mouse_pos:Vector2 = get_global_mouse_position()
+	var query:PhysicsPointQueryParameters2D = PhysicsPointQueryParameters2D.new()
 	query.position = mouse_pos
 	query.collide_with_bodies = true
 	var result = get_world_2d().direct_space_state.intersect_point(query)
@@ -58,15 +56,11 @@ func get_body_under_mouse():
 	return null
 
 func _get_displacements(sb: SoftBody2D,rg: RigidBody2D):
-	var i:int=0
 	for child in sb.get_children():
 		if child is RigidBody2D:
 			bones.append(child)
-			i+=1
-	
 	var current_mainbone_pos = rg.get_global_transform().origin
-	i=0
 	displacements.resize(bones.size())
-	for bone in bones:
+	for i in bones.size():
+		var bone:RigidBody2D = bones[i]
 		displacements[i] = bone.get_global_transform().origin - current_mainbone_pos
-		i+=1
--- Scripts/Gyroscope2D.gd
+++ Scripts/Gyroscope2D.gd
@@ -1,14 +1,28 @@
 extends Node2D
 
+@export_group("Debug","debug_")
 @export var debug_gravity: Label
 @export var debug_gravity_direction: Label
 @export var debug_accelerometer: Label
-@export var degub_accelerometerLength: Label
-@export var fpscounter: Label
+@export var debug_accelerometerLength: Label
+@export var debug_fpscounter: Label
+
+@export_group("Acceleration","accel_")
+@export var accel_threshold: float = 10
+@export var accel_min: float = 0
+@export var accel_max: float = 50
+var accel_threshold_sq: float
+var accel_min_sq: float
+var accel_max_sq: float
+
+func _ready():
+	accel_threshold_sq = accel_threshold * accel_threshold
+	accel_min_sq = accel_min * accel_min
+	accel_max_sq = accel_max * accel_max
 
 func _physics_process(_delta):
 	var InputGravity: Vector3 = Input.get_gravity() #directional vector of gravity
-	var InputGyroscope: Vector3 =Input.get_gyroscope() #angular velocity
+	var InputGyroscope: Vector3 = Input.get_gyroscope() #angular velocity
 	var InputAcceleration: Vector3 = Input.get_accelerometer() #spacial velocity
 	var InputMagnetometer: Vector3 = Input.get_magnetometer()
 	debug_gravity.text = "Gyroscope value : " + "X: " + str(roundf(InputGravity.x)) + " Y: " + str(roundf(InputGravity.y)) + " Z: " + str(roundf(InputGravity.z))
@@ -16,7 +30,7 @@ func _physics_process(_delta):
 	#Gravity direction
 	var OutputGravity: Vector2 = Vector2(InputGravity.x,-InputGravity.y)
 	debug_gravity_direction.text = "Gravity Direction : " + "X: " + str(roundf(OutputGravity.x)) + " Y: " + str(roundf(OutputGravity.y))
-	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, OutputGravity.normalized())
+	#PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, OutputGravity.normalized())
 	
 	#Gravity force relative to a rest pose when X&Y = 0
 	var OutputGravityLength: Vector2 = OutputGravity*100 #Convert m/s to px/s
@@ -24,12 +38,17 @@ func _physics_process(_delta):
 	
 	#Acceleration
 	#Negate gravity to get relative acceleration of the phone
-	var OutputAcceleration: Vector3 = InputGravity - InputAcceleration
-	debug_accelerometer.text = "Accelerometer : " + "X: " +str(roundf(OutputAcceleration.x)) + " Y: " + str(roundf(OutputAcceleration.y)) + " Z: " + str(roundf(OutputAcceleration.z))
-	degub_accelerometerLength.text = "Accel Length|Squared : " + str(roundf(OutputAcceleration.length())) + " | " + str(roundf(OutputAcceleration.length_squared()))
+	var InputAccelerationPure: Vector3 = InputGravity - InputAcceleration
+	var OutputAcceleration: Vector2 = Vector2(InputGravity.x - InputAcceleration.x, InputGravity.y - InputAcceleration.y)
+	var OutputAcceleration_length_sq = InputAccelerationPure.length_squared()
+	debug_accelerometer.text = "Accelerometer : " + "X: " +str(roundf(InputAccelerationPure.x)) + " Y: " + str(roundf(InputAccelerationPure.y)) + " Z: " + str(roundf(InputAccelerationPure.z))
+	debug_accelerometerLength.text = "Accel Length|Squared : " + str(roundf(InputAccelerationPure.length())) + " | " + str(roundf(OutputAcceleration_length_sq))
+	var accel_magnitude:float = max(0.0,OutputAcceleration_length_sq - accel_threshold_sq)
+	var accel_dir: Vector2 = OutputAcceleration.normalized() * accel_magnitude
+	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, OutputGravity.normalized() + accel_dir)
 
 func _process(_delta):
-	fpscounter.text = "FPS: %d" % Engine.get_frames_per_second()
+	debug_fpscounter.text = "FPS: %d" % Engine.get_frames_per_second()
 
 func _on_reset_button_pressed():
 	get_tree().reload_current_scene()
